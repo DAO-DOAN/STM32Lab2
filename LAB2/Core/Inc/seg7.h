dc68363a47f49db517b8bb6b15b78ae2276816fa
DESKTOP-QL5HS61\Legion 5 <doandao1603@gmail.com>
2024-10-09 13:27:24 +0700

    First Set Up

---
+++ LAB2/Core/Inc/seg7.h
@@ -0,0 +1,14 @@
+/*
+ * seg7.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: Legion 5
+ */
+
+#ifndef INC_SEG7_H_
+#define INC_SEG7_H_
+
+#include "main.h"
+void display7SEG(int num);
+
+#endif /* INC_SEG7_H_ */
